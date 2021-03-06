f48d7e4555e0347217e5d4090afeb2317dd16a0e
thel <eliothal@gmail.com>
2021-03-06 14:49:53 +0100

    i

--- Täglich_Inkrementell.sh
+++ Täglich_Inkrementell.sh
@@ -5,7 +5,8 @@ ssh root@192.168.80.132 bash /var/backup/backupscript.sh # Zieht die Dateien von
 # Führt das Script auf dem Computer backupclient2 aus
 ssh root@192.168.80.133 bash /var/backup/backupscript.sh # Zieht die Dateien von dem 2.Client auf den Server im Verzeichnis "/home/backup/tmp/{aktuelles_datum}/{aktuelles_datum}backupclient2.tar.gz"
 
+mkdir -p /home/backup/backup/client1/$(date +%Y-%m-%d)/
+mkdir -p /home/backup/backup/client2/$(date +%Y-%m-%d)/
+rsync "$ziel/backupclient1.tar.gz" /home/backup/backup/client1/$(date +%Y-%m-%d)/ #--link-dest="/home/backup/backup/client1/tag/vordere_sicherung"
 
-rsync "$ziel/backupclient1.tar.gz" /home/backup/backup/client1/tag/ #--link-dest="/home/backup/backup/client1/tag/vordere_sicherung"
-
-rsync "$ziel/backupclient2.tar.gz" /home/backup/backup/client2/tag/ #--link-dest="/home/backup/backup/client2/tag/vordere_sicherung"
+rsync "$ziel/backupclient2.tar.gz" /home/backup/backup/client2/$(date +%Y-%m-%d)/ #--link-dest="/home/backup/backup/client2/tag/vordere_sicherung"
