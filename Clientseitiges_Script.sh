b859c53a6a2b5680a8833612628786d06c215d0a
thel <eliothal@gmail.com>
2021-03-08 19:59:43 +0100

    Inkrementell Differenziell

--- Clientseitiges_Script.sh
+++ Clientseitiges_Script.sh
@@ -3,4 +3,4 @@ ziel="/home/thel/tmp/$(date +%Y-%m-%d)"
 filename="$(date +%Y-%m-%d)_backupclient1.tar.gz"
 tar -czvf $filename /home/thel/Documents /home/thel/Pictures
 ssh backup@192.168.80.131 mkdir -p $ziel
-rsync -av -e ssh $filename backup@192.168.80.131:$ziel
+rsync -av -e ssh $filename thel@192.168.80.131:$ziel
--- Täglich_Inkrementell.sh
+++ Täglich_Inkrementell.sh
@@ -1,12 +1,40 @@
 #!/bin/bash
+# t=Täglich Inkrementell
+# w=Wöchentlich Differenziell
+# m=Monatlich Vollsicherung
+# j=Jahr Vollsicherung
+$1
+$2
 ziel="/home/thel/tmp/$(date +%Y-%m-%d)"
 # Führt das Script auf dem Computer backupclient1 aus
 ssh root@192.168.80.132 bash /var/backup/backupscript.sh # Zieht die Dateien von dem 1.Client auf den Server im Verzeichnis "/home/backup/tmp/{aktuelles_datum}/{aktuelles_datum}backupclient1.tar.gz"
 # Führt das Script auf dem Computer backupclient2 aus
 #ssh root@192.168.80.133 bash /var/backup/backupscript.sh # Zieht die Dateien von dem 2.Client auf den Server im Verzeichnis "/home/backup/tmp/{aktuelles_datum}/{aktuelles_datum}backupclient2.tar.gz"
 
-mkdir -p /home/thel/backup/client1/$(date +%Y-%m-%d)/
-#mkdir -p /home/thel/backup/client2/$(date +%Y-%m-%d)/
-rsync "$ziel/backupclient1.tar.gz" /home/thel/backup/client1/$(date +%Y-%m-%d)/ #--link-dest="/home/backup/backup/client1/tag/vordere_sicherung"
-
-#rsync "$ziel/backupclient2.tar.gz" /home/thel/backup/client2/$(date +%Y-%m-%d)/ #--link-dest="/home/backup/backup/client2/tag/vordere_sicherung"
+if [ $1 -eq "t" ]; then
+    echo 'Tägliche Sicherung wird gestartet'
+    mkdir -p /home/thel/backup/client1/tag/$(date +%Y-%m-%d)/
+    #mkdir -p /home/thel/backup/client2/tag/$(date +%Y-%m-%d)/
+    rsync "$ziel/$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client1/tag/$(date +%Y-%m-%d)/ --link-dest=$2
+    #rsync "$ziel/$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client2/tag/$(date +%Y-%m-%d)/ --link-dest=$2
+elif [ $1 -eq "w" ]; then
+    echo 'Wöchentliche Sicherung wird gestartet'
+    mkdir -p /home/thel/backup/client1/woche/$(date +%Y-%m-%d)/
+    #mkdir -p /home/thel/backup/client2/woche/$(date +%Y-%m-%d)/
+    rsync "$ziel/$(date +%Y-%m-%d)_$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client1/woche/$(date +%Y-%m-%d)/ --link-dest=$2
+    #rsync "$ziel/$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client2/woche/$(date +%Y-%m-%d)/ --link-dest=$2
+elif [ $1 -eq "m" ]; then
+    echo 'Monatliche Sicherung wird gestartet'
+    mkdir -p /home/thel/backup/client1/monat/$(date +%Y-%m-%d)/
+    #mkdir -p /home/thel/backup/client2/monat/$(date +%Y-%m-%d)/
+    rsync "$ziel/$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client1/monat/$(date +%Y-%m-%d)/
+    #rsync "$ziel/$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client2/monat/$(date +%Y-%m-%d)/
+elif [ $1 -eq "j" ]; then
+    echo 'Jährliche Sicherung wird gestartet'
+    mkdir -p /home/thel/backup/client1/jahr/$(date +%Y-%m-%d)/
+    #mkdir -p /home/thel/backup/client2/jahr/$(date +%Y-%m-%d)/
+    rsync "$ziel/$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client1/jahr/$(date +%Y-%m-%d)/
+    #rsync "$ziel/$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client2/jahr/$(date +%Y-%m-%d)/
+else
+  echo 'fehlerhafte Eingabe'
+fi
