2d68063ebe520bed0bba6f24db42a14dd2760a40
thel <eliothal@gmail.com>
2021-03-08 20:02:50 +0100

    -eq = ==

--- Täglich_Inkrementell.sh
+++ Täglich_Inkrementell.sh
@@ -11,25 +11,25 @@ ssh root@192.168.80.132 bash /var/backup/backupscript.sh # Zieht die Dateien von
 # Führt das Script auf dem Computer backupclient2 aus
 #ssh root@192.168.80.133 bash /var/backup/backupscript.sh # Zieht die Dateien von dem 2.Client auf den Server im Verzeichnis "/home/backup/tmp/{aktuelles_datum}/{aktuelles_datum}backupclient2.tar.gz"
 
-if [ $1 -eq "t" ]; then
+if [ $1 == "t" ]; then
     echo 'Tägliche Sicherung wird gestartet'
     mkdir -p /home/thel/backup/client1/tag/$(date +%Y-%m-%d)/
     #mkdir -p /home/thel/backup/client2/tag/$(date +%Y-%m-%d)/
     rsync "$ziel/$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client1/tag/$(date +%Y-%m-%d)/ --link-dest=$2
     #rsync "$ziel/$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client2/tag/$(date +%Y-%m-%d)/ --link-dest=$2
-elif [ $1 -eq "w" ]; then
+elif [ $1 == "w" ]; then
     echo 'Wöchentliche Sicherung wird gestartet'
     mkdir -p /home/thel/backup/client1/woche/$(date +%Y-%m-%d)/
     #mkdir -p /home/thel/backup/client2/woche/$(date +%Y-%m-%d)/
     rsync "$ziel/$(date +%Y-%m-%d)_$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client1/woche/$(date +%Y-%m-%d)/ --link-dest=$2
     #rsync "$ziel/$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client2/woche/$(date +%Y-%m-%d)/ --link-dest=$2
-elif [ $1 -eq "m" ]; then
+elif [ $1 == "m" ]; then
     echo 'Monatliche Sicherung wird gestartet'
     mkdir -p /home/thel/backup/client1/monat/$(date +%Y-%m-%d)/
     #mkdir -p /home/thel/backup/client2/monat/$(date +%Y-%m-%d)/
     rsync "$ziel/$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client1/monat/$(date +%Y-%m-%d)/
     #rsync "$ziel/$(date +%Y-%m-%d)_backupclient1.tar.gz" /home/thel/backup/client2/monat/$(date +%Y-%m-%d)/
-elif [ $1 -eq "j" ]; then
+elif [ $1 == "j" ]; then
     echo 'Jährliche Sicherung wird gestartet'
     mkdir -p /home/thel/backup/client1/jahr/$(date +%Y-%m-%d)/
     #mkdir -p /home/thel/backup/client2/jahr/$(date +%Y-%m-%d)/
