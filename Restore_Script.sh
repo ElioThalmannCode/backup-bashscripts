127a3c2b3843a77fc982ac3b4e14872998d28f69
thel <eliothal@gmail.com>
2021-03-08 21:12:59 +0100

    s

--- Restore_Script.sh
+++ Restore_Script.sh
@@ -2,5 +2,5 @@
 # Elio Thalmann
 # Modul 143
 # Restore Script
-rsync -av /home/thel/backup/$1 root@$2:/var/backup
-ssh root@$2 tar -xf /home/thel/backup/$1
+rsync -av $1 root@$2:/var/backup
+ssh root@$2 tar -xf $1
