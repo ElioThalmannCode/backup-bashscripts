6433316b96ae04fb2ebee7101760a392ebe842c6
thel <eliothal@gmail.com>
2021-03-08 11:41:28 +0100

    l

--- Clientseitiges_Script.sh
+++ Clientseitiges_Script.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 ziel="/home/thel/tmp/$(date +%Y-%m-%d)"
-filename="backupclient1.tar.gz"
+filename="$(date +%Y-%m-%d)_backupclient1.tar.gz"
 tar -czvf $filename /home/thel/Documents /home/thel/Pictures
 ssh backup@192.168.80.131 mkdir -p $ziel
 rsync -av -e ssh $filename backup@192.168.80.131:$ziel
