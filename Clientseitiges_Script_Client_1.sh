627a69f9ca7b526a5ee9017a970a40618657f4d5
thel <eliothal@gmail.com>
2021-03-08 22:00:30 +0100

    thel backup

--- Clientseitiges_Script_Client_1.sh
+++ Clientseitiges_Script_Client_1.sh
@@ -10,6 +10,6 @@ filename="$(date +%Y-%m-%d)_backupclient1.tar.gz"
 # Klebt Dateien zusammen und komprimiert sie.
 tar -czvf $filename /home/thel/Documents /home/thel/Pictures # <---- Hier kann man die Daten auswählen
 # Erstellt einen Ordner mit dem aktuellen Datum
-ssh backup@192.168.80.131 mkdir -p $ziel
+ssh thel@192.168.80.131 mkdir -p $ziel
 # Kopiert das tar.gz Archiv mit einer sicheren SSH Verbindung auf den Backupserver
 rsync -av -e ssh $filename thel@192.168.80.131:$ziel
