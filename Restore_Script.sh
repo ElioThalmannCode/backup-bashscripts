78bde72637ed9e36cb702864f858cac3cf099d22
thel <eliothal@gmail.com>
2021-03-09 13:04:05 +0100

    s

--- Restore_Script.sh
+++ Restore_Script.sh
@@ -3,4 +3,9 @@
 # Modul 143
 # Restore Script
 rsync -e ssh -av $1 root@$2:/var/backup
-ssh root@$2:/ tar -xf /var/backup/$3
+ssh root@$2 "cd /; tar -xf /var/backup/$3"
+
+# rsync -e ssh -av /home/thel/backup/client1/jahr/2021-03-08/2021-03-08_backupclient1.tar.gz root@192.168.80.132/var/backup
+# ssh root@192.168.80.132 
+# cd /
+# tar -xf /var/backup/$""
